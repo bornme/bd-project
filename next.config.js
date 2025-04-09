482dda6b900d8eaa6d59af30905ba5d5693d7dc4
bornme <83337979+bornme@users.noreply.github.com>
2025-04-09 08:49:20 +0000

    Simplify next.config.js for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 const nextConfig = {
   reactStrictMode: false, // Disable Strict Mode to prevent double rendering
   images: { unoptimized: true },
-  basePath: process.env.NODE_ENV === "production" ? "/bd-project" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/bd-project/" : "",
 };
-
-if (process.env.NODE_ENV === "production") {
-  nextConfig.output = "export";
-}
 
 module.exports = nextConfig;
